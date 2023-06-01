f5d2dad074cc3c1f03425b32f6ef4220835cbdd5
G284532022 <105624487+G284532022@users.noreply.github.com>
2023-06-01 16:08:49 +0900

    数当て完成

--- kazuate.js
+++ kazuate.js
@@ -16,21 +16,24 @@ function hantei() {
   Number('yoso');
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   kaisu=kaisu+1;
-  let su =document.querySelector('span#kaisu');
-  su.textContent=kaisu;
-  let n =document.querySelector('span#answer');
-  n.textContent=yoso;
+ 
+  let syouri=0;
 
   // 課題3-1: 正解判定する;
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
   let t = document.querySelector('p#result');
- if(kaisu<4){
+ if(kaisu<4&&syouri!== 1){
+   let su =document.querySelector('span#kaisu');
+   su.textContent=kaisu+"回目の予想:";
+   let n =document.querySelector('span#answer');
+   n.textContent=yoso;
     if(yoso==kotae){
-      t.textContent="正解です.おめでとう!"; 
-    }if(yoso<kotae){
+      t.textContent="正解です.おめでとう!";
+      syouri=1;
+    }else if(yoso<kotae){
       t.textContent="まちがい.答えはもっと大きいですよ.";
-    }if(yoso>kotae){
+    }else{
       t.textContent="まちがい.答えはもっと小さいですよ.";
     }
   }else{
