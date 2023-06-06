f333760396015dcab3f13d34192087755be8d691
G284532022 <105624487+G284532022@users.noreply.github.com>
2023-06-07 01:58:17 +0900

    変更

--- project.js
+++ project.js
@@ -2,9 +2,27 @@
 let dd = [{tosi:"カイロ", id:"360630"},{tosi:"モスクワ", id:"524901"},{tosi:"ヨハネスブルク", id:"993800"},{tosi:"北京", id:"1816670"},{tosi:"東京", id:"1850147"}
 ,{tosi:"シンガポール", id:"1880252"},{tosi:"シドニー", id:"2147714"},{tosi:"ロンドン", id:"2643743"},{tosi:"パリ", id:"2968815"}
 ,{tosi:"リオデジャネイロ", id:"3451189"},{tosi:"ニューヨーク", id:"5128581"},{tosi:"ロサンゼルス", id:"5368361"}]
+let r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11;
+r0=document.getElementById("a");
+r1=document.getElementById("b");
+r2=document.getElementById("c");
+r3=document.getElementById("d");
+r4=document.getElementById("e");
+r5=document.getElementById("f");
+r6=document.getElementById("g");
+r7=document.getElementById("h");
+r8=document.getElementById("i");
+r9=document.getElementById("j");
+r10=document.getElementById("k");
+r11=document.getElementById("l");
+
 let b = document.querySelector('#print');
-b.addEventListener('click', sendRequest);
-function sendRequest(){
+b.addEventListener('click', erabareta);
+function erabareta(){
+  let tumo =[r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11];
+  sendRepuest(dd[0].id);
+}
+function sendRequest(y){
   // URL を設定
   let d =document.querySelector('input[name="toshi"]');
   let y =parseInt(d.value);
@@ -28,21 +46,13 @@ function showResult(resp) {
   }
   let f=document.querySelector('p#ansewer');
   f.textContent=data.name+'の最高気温です'+data.main.temp_max+'です。最低気温は'+data.main.temp_min+'です。'
-
-  // data をコンソールに出力
-  console.log(data);
-
-  // data.x を出力
-  console.log(data.x);
 }
-
 // 通信エラーが発生した時の処理
 function showError(err) {
   console.log(err);
   let f=document.querySelector('p#ansewer');
   f.taxtContent='そんなIDありません'
 }
-
 // 通信の最後にいつも実行する処理
 function finish() {
   console.log('Ajax 通信が終わりました');
